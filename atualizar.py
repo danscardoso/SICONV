3de30edea4af82d5958acf1d0d3874ade42f69c9
Dan Cardoso <danscardoso@yahoo.com.br>
2019-09-10 01:47:36 -0300

    Adicionada o argumento de port para poder modificar ele tambem na conexao. Percebi que tem uma view que nao funciona, a que tras as informacoes das acoes orcamentarias

--- atualizar.py
+++ atualizar.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 import psycopg2                                           # conexão com o banco de dados
-from urllib.request import urlretrieve                    # download de um recurso via URL
+import requests                                           # download de um recurso via URL
 import zipfile                                            # para fazer o unzip do arquivo baixado
 from time import ctime                                    # para a pasta temporaria, log na CLI e adicionar informação da data da atualização
 from getpass import getpass                               # leitura sem echo em tela (caso seja desejado esconder a senha e nao deixar ela no script)
@@ -43,18 +43,14 @@ parser.add_argument('--viewFolder', nargs=1, default="./SQL/ViewsServidor",
 parser.add_argument('--schemaFile', nargs=1, default="./SQL/create_schema.sql",
                    help='Altere esta flag para mudar o arquivo fonte do schema. Default: ./SQL/create_schema.sql ')
 
-#Endereço do host do banco de dados
+# Argumentos para permitir o usuario mudar a conexão na invocação do programa
+# host, dbname e port podem ser mudados
 parser.add_argument('--host', nargs=1, default="127.0.0.1",
                    help='Altere esta flag para mudar o host do banco de dados a ser alterado. Default: 127.0.0.1 ')
-
-#Endereço do arquivo create schema
 parser.add_argument('--dbname', nargs=1, default="siconv",
                    help='Altere esta flag para mudar o arquivo da database a ser mudada. Default: siconv ')
-
-#Informe para manter os arquivos baixados
-parser.add_argument('--aproveitarDownload', action='store_true',
-                   help='Informe esta flag para aproveitar os arquivos baixado em uma execução anterior ao inves de baixar e descompactar de novo')
-
+parser.add_argument('--port', nargs=1, default="5432",
+                   help='Altere esta flag para mudar a porta que o host escuta. Default: 5432 ')
 
 
 args = parser.parse_args()
@@ -65,11 +61,11 @@ truncate_flag           = args.truncate
 generateCSV_flag        = args.gerarCSV
 keepDownload_flag       = args.keepDownload
 semViews_flag           = args.semViews
-aproveitarDownload_flag = args.aproveitarDownload
 pasta_views    = ( args.viewFolder[0] if isinstance(args.viewFolder, list) else args.viewFolder )
 arquivo_schema = ( args.schemaFile[0] if isinstance(args.schemaFile, list) else args.schemaFile )
 host           = ( args.host[0]       if isinstance(args.host, list)       else args.host )
 dbname         = ( args.dbname[0]     if isinstance(args.dbname, list)     else args.dbname )
+port           = ( args.port[0]       if isinstance(args.dbname, list)     else args.port )
 
 if reset_flag and truncate_flag:
     raise Exception('Ou reseta ou trunca o schema, nao faca ambos!')
@@ -77,70 +73,71 @@ if reset_flag and truncate_flag:
 if generateCSV_flag and semViews_flag:
     raise Exception('Preciso gerar as views para gerar o CSV')
 
-# testar a conexão
+# testar a conexão, se a conexão der ruim (senha errada ou qualquer coisa), nem se dê ao trabalho de
+# baixar os dados
 try: 
     usuario = input("USUARIO DO POSTGRES: ")
     senha = getpass() 
-    conn_string = "host='"+host+"' dbname='"+dbname+"' user='"+usuario+"' password='"+senha+"'"
-
+    conn_string = "host='"+host+"' dbname='"+dbname+"' user='"+usuario+"' password='"+senha+"' port='"+port+"'"
+    #
     #caso deixar a senha no script seja aceitável pode-se trocar as tres linhas de cima por esta
     #conn_string = "host='"+host+"' dbname='"+dbname+"' user='admin' password='admin'"
-
+    #
     print(" *** " + ctime() + " Testando conexao...")
     conn = psycopg2.connect(conn_string)    
     conn.close()
 except Exception as error: 
     print('ERROR', error) 
     exit()
-print(" *** " + ctime() + " Conexão realizada com sucesso")
+print(" *** " + ctime() + " Conexão testada com sucesso.")
 
-#Sabendo que a conexão funciona, agora dá para de fato baixar os dados e levar a cabo a atualização
+###############################################################################
+# Sabendo que a conexão funciona, agora faz sentido baixar os dados e levar a cabo a atualização
+# Agora faço etapas preliminares>
+#   -> Criar pasta temporaria (caso não exista)
+#   -> Baixar
+#   -> Descompactar
 
-nome_pasta_temp = getcwd() + "/__pasta_temporaria__" + ctime()[4:10] #'Jul 17' por exemplo
+nome_pasta_temp = getcwd() + "/__pasta_temporaria__" + ctime()[4:10].replace(" ", "") #'_pasta_temporaria__Jul17' por exemplo
 
 # Se tem downloads do arquivo, eu vou apagar e refazer por que não tenho muito como garantir que a validade dessa pasta
-# Se aprender a validar esta pasta, é possível ignorar essa parte, a do download e a da descompactação
-if path.exists(nome_pasta_temp) and not aproveitarDownload_flag:
-    print (" *** " + ctime() + " Apagando a pasta com downloads no processo sem sucesso...")
+# Então depois disso tem uma pasta vazia com o nome do dia
+if path.exists(nome_pasta_temp):
     for file in listdir(nome_pasta_temp):
         remove(nome_pasta_temp + "/" + file)
+else:
+    mkdir(nome_pasta_temp)
 
-if not aproveitarDownload_flag:
 
-    if path.exists(nome_pasta_temp) is False:
-        mkdir(nome_pasta_temp)
+print(" *** " + ctime() + " Baixando dados...")
 
-    print(" *** " + ctime() + " Baixando dados...")
-    tentativas = 0
-    while tentativas < 3:
-        try:
-            urlretrieve( "http://plataformamaisbrasil.gov.br/images/docs/CGSIS/csv/siconv.zip" ,  nome_pasta_temp + "/dados_siconv.zip")
-            break
-        except:
-            tentativas += 1
-    if tentativas == 3:
-        print ("A fonte parece estar fora do ar")
-        exit()
-    else:
-        print(" *** " + ctime() + " Dados baixados.")
+#Baixando os dados quebrando o download em um stream (memória)
+with requests.get("http://plataformamaisbrasil.gov.br/images/docs/CGSIS/csv/siconv.zip", stream=True) as r:
+    r.raise_for_status()
+    with open(nome_pasta_temp + "/dados_siconv.zip", 'wb') as f:
+        for chunk in r.iter_content(chunk_size=8192): 
+            if chunk: # filter out keep-alive new chunks
+                _ = f.write(chunk)
+                # f.flush()
 
-    #Unzipar os arquivos
-    print(" *** " + ctime() + " Descompactando arquivo baixado...")
-    try:
-        with zipfile.ZipFile(nome_pasta_temp + "/dados_siconv.zip","r") as zip_ref:
-            zip_ref.extractall(nome_pasta_temp + "/")
-    except:
-        print (" *** " + ctime() + " Erro! Arquivo não encontrado.")
-        exit()
-    print (" *** " + ctime() + " Descompactação finalizada")
-    
-#Atualizando o banco
+# Descompactar
+print(" *** " + ctime() + " Descompactando arquivo baixado...")
+with zipfile.ZipFile(nome_pasta_temp + "/dados_siconv.zip","r") as zip_ref:
+    zip_ref.extractall(nome_pasta_temp + "/")
+print (" *** " + ctime() + " Descompactação finalizada")
+
+###############################################################################
+# Com os dados em mãos, vou conectar no banco
+
+
+# Conectando com o banco
 conn = psycopg2.connect(conn_string)
 cur = conn.cursor()
 
 if reset_flag:
     print (" *** " + ctime() + " Derrubando e recriando o schema ...")
-    
+    #
+    # Abro o arquivo que define o schema da tabela, leio ele todo, quebro em 
     with open(arquivo_schema) as arq_schema:
         comandos = arq_schema.read()
         comandos = comandos.upper().replace("SICONV", schema_alvo )
@@ -152,7 +149,6 @@ if reset_flag:
             except psycopg2.ProgrammingError:
                 continue
     conn.commit()
-
 elif truncate_flag:
     print (" *** " + ctime() + " Truncando as tabelas...")
     cur.execute('TRUNCATE TABLE '+ schema_alvo +'.consorcios')
@@ -202,12 +198,12 @@ if reset_flag or truncate_flag:
     lista_comandos.append( ('COPY '+ schema_alvo +'.proposta                   FROM STDIN ( FORMAT(\'csv\'), DELIMITER(\';\'), QUOTE(E\'\"\'), HEADER)', nome_pasta_temp + '/siconv_proposta.csv'))
     lista_comandos.append( ('COPY '+ schema_alvo +'.prorroga_oficio            FROM STDIN ( FORMAT(\'csv\'), DELIMITER(\';\'), QUOTE(E\'\"\'), HEADER)', nome_pasta_temp + '/siconv_prorroga_oficio.csv'))
     lista_comandos.append( ('COPY '+ schema_alvo +'.termo_aditivo              FROM STDIN ( FORMAT(\'csv\'), DELIMITER(\';\'), QUOTE(E\'\"\'), HEADER)', nome_pasta_temp + '/siconv_termo_aditivo.csv'))
-
+    #
     print (" *** " + ctime() + " Inserindo dados ...")
     for (comando, endereco) in lista_comandos:
         print(comando, endereco)
         cur.copy_expert(comando, open(endereco))
-
+    #
     conn.commit()
 
 #fazendo as views a menos que o usuario explicitamente não peça isso
@@ -218,10 +214,11 @@ if not semViews_flag :
             with open(pasta_views + "/" + file ) as arquivo_view:
                 comandos = arquivo_view.read()
                 comandos = comandos.upper().replace("SICONV.", schema_alvo + "." )
-
+                #
                 print (" *** " + ctime() + " Implementando view " + file )
                 for comando in comandos.split(";"):
                     try:
+                        print(comando)
                         cur.execute(comando)
                         conn.commit()
                     except psycopg2.ProgrammingError:
